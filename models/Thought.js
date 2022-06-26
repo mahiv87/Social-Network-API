41a5e7d2a5133c01259d53d19f42ce4499b665ac
Marcus Herrera <maherreraiv1987@gmail.com>
2022-06-25 18:17:06 -0600

    Thought model code added

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+	{
+		thoughtText: {
+			type: String,
+			required: true,
+			maxLength: 280
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now(),
+			get: format
+		},
+		username: {
+			type: String,
+			required: true
+		},
+		reactions: [reactionSchema]
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true
+		}
+	}
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+	return this.reactions.length;
+});
+
+function format(date) {
+	return formatDate(date);
+}
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
