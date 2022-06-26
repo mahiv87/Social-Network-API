19754711d8650de813b6960ab45316060e069700
Marcus Herrera <maherreraiv1987@gmail.com>
2022-06-25 18:28:23 -0600

    Reaction subdoc code added

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+	{
+		reactionId: {
+			type: Schema.Types.ObjectId,
+			default: () => new Types.ObjectId()
+		},
+		reactionBody: {
+			type: String,
+			required: true,
+			maxLength: 280
+		},
+		username: {
+			type: String,
+			required: true
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now(),
+			get: format
+		}
+	},
+	{
+		toJSON: {
+			getters: true
+		},
+		id: false
+	}
+);
+
+function format(date) {
+	return formatDate(date);
+}
+
+module.exports = reactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -23,7 +23,8 @@ const thoughtSchema = new Schema(
 		toJSON: {
 			virtuals: true,
 			getters: true
-		}
+		},
+		id: false
 	}
 );
 
